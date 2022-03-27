df6dfec09546d554cd1605c27e43046569636b3a
Алена Пестова <aspestova_1@edu.hse.ru>
2022-03-27 18:31:50 +0300

    Генерация лстм с предобученными эмбеддингами

--- train_lstm_full.sh
+++ train_lstm_full.sh
@@ -4,3 +4,9 @@
 #было обучено на Kaggle
 
 !python3 src/train_lstm.py -i data/full_data_clean_reversed.csv -o models/lstms/ -e 60 -v models/lstms/lstm_vocab.json -d "cuda" -b 512
+
+# с предобученнными эмбеддингами
+!python3 src/train_lstm.py -i data/full_data_clean_reversed.csv -o models/lstms/ -e 60 -v models/lstms/lstm_vocab.json -d "cuda" -b 512
+
+!wget https://storage.yandexcloud.net/natasha-navec/packs/n..
+!python3 src/train_lstm_with_pretrained_emb.py -i data/full_data_clean_reversed.csv -e 60 -b 400 -o models/lstms/ -e 60 -v models/lstms/lstm_vocab_emb.json -d "cuda" -n "navec_hudlit_v1_12B_500K_300d_100q.tar"
